74987be63240f3199a4e80a7c34c35861ffa382d
Lothar Märkle <lothar.maerkle@ecm4u.de>
2012-09-05 10:01:51 +0200

    upload using the upload webscript instead of webdav. this way the noderef of the newly created file will be returned

--- bin/alfUpload.sh
+++ bin/alfUpload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -x
+# set -x
 # param section
 
 # source function library
@@ -11,7 +11,7 @@ ALFTOOLS_BIN=`dirname "$0"`
 function __show_command_options() {
   echo "  command options:"
   echo "    -n NAME        filename to use for Alfresco"
-  echo
+  echo "    -m MIMETYPE    mime type of the added file"
 }
 
 # intended to be replaced in command script
@@ -24,7 +24,7 @@ function __show_command_arguments() {
 # intended to be replaced in command script
 function __show_command_explanation() {
   echo "  command explanation:"
-  echo "    the alfUpload.sh command adds content to Alfresco"
+  echo "    the alfUpload.sh command adds content to Alfresco. If there is already a document, its contents will be updated."
   echo
   echo "  usage examples:"
   echo
@@ -34,16 +34,18 @@ function __show_command_explanation() {
   echo "  ./alfUpload.sh ./local/file.pdf workspace://SpacesStore/1234-1234-1234-1234"
   echo "     --> uplodas the local file at ./local/file.pdf to the space given by the nodeRef"
   echo
-  echo "  ./alfUpload.sh -n "filename.pdf" - /some/repo/path/to/space"
+  echo "  cat sth.pdf | ./alfUpload.sh -n "filename.pdf" - /some/repo/path/to/space"
   echo "     --> uploads content read from stdin and saves it to a file at /some/repo/path/to/space/filename.pdf"
   
 }
 
 
 # command local options
-ALF_CMD_OPTIONS="${ALF_GLOBAL_OPTIONS}n:"
+ALF_CMD_OPTIONS="${ALF_GLOBAL_OPTIONS}n:m:"
 ALF_CONTENT_PROP=cm:content
 ALF_FILENAME=""
+ALF_CONTENT_TYPE="cm:content"
+ALF_MIMETYPE=""
 
 function __process_cmd_option() {
   local OPTNAME=$1
@@ -87,6 +89,7 @@ then
   echo "  curl opts: $ALF_CURL_OPTS"
   echo "  local file: $ALF_LOCAL_FILE"
   echo "  alf filename: $ALF_FILENAME"
+  echo "  alf url: $ALF_URL"
 fi
 
 if [[ "$ALF_URL" == "" ]]
@@ -95,22 +98,51 @@ then
   exit 1
 fi
 
+if [[ "$ALF_MIMETYPE" != "" ]]
+then
+  ALF_MT_ARG=";type=$ALF_MIMETYPE"
+else
+  ALF_MT_ARG=""
+fi
 
 if __is_noderef $ALF_URL
 then
-  __split_noderef $ALF_URL
-  __encode_url_param $UUID
-  ENC_UUID=$ENCODED_PARAM
-  __encode_url_param $STORE
-  ENC_STORE=$ENCODED_PARAM
-  __encode_url_param $PROTOCOL
-  ENC_PROTOCOL=$ENCODED_PARAM
- 
-#  curl $ALF_CURL_OPTS -u $ALF_UID:$ALF_PW $ALF_EP/service/api/node/$ENC_PROTOCOL/$ENC_STORE/$ENC_UUID/content
+  curl $ALF_CURL_OPTS -u $ALF_UID:$ALF_PW  --form "filedata=@$ALF_LOCAL_FILE;filename=${ALF_FILENAME}${ALF_MT_ARG}" --form "destination=$ALF_URL" $ALF_EP/service/api/upload | $ALF_JSHON -e nodeRef -u
 else
-  __encode_url_path $ALF_URL/$ALF_FILENAME
-  curl $ALF_CURL_OPTS -u $ALF_UID:$ALF_PW -H 'Content-Type:application/octet-stream' --data-binary @$ALF_LOCAL_FILE -X PUT $ALF_EP/webdav/$ENCODED_PATH
+  ALF_COMPANY_HOME_REF=`$ALFTOOLS_BIN/alfGetCompanyHomeNodeRef.sh`
+
+  curl $ALF_CURL_OPTS -u $ALF_UID:$ALF_PW  --form "filedata=@$ALF_LOCAL_FILE;filename=${ALF_FILENAME}${ALF_MT_ARG}" --form "destination=$ALF_COMPANY_HOME_REF" --form "uploaddirectory=$ALF_URL" $ALF_EP/service/api/upload | $ALF_JSHON -e nodeRef -u
+
 fi
+
+
+
+exit
+
+#
+#upload webscript parameter description:
+#
+#UPDATE
+#updatenoderef: (required) used to update the contents of an existing node known by its noderef
+#description: (optional) used with updatenoderef, only used if updatenoderef is set and a new version is generated. it is the check in comment. it is not the cm:description field
+#majorversion: (optional, defaults to false) used with updatenoderf / used to create a new version
+
+
+#CREATE in repo
+#destination: (required)a noderef which becomes the parent of the newly created/updated node
+#aspects: (optional) comma separeted list of aspects to add to the newly created or updated node
+#contentype: (optional)set the alfresco type on new node creation
+#uploaddirectory: (optional) a name path anchored from the noderef given by destination. can be used to set the parent by name path
+#overwrite: (optional) replace the exiting node if any - only evaluated if the node has the cm:versionable aspect
+
+#ALL
+#filedata: (required) file contents, form field parameter 'filename' is required to set the filename, form field parameter type is optional used to set the mimetype
+
+
+
+
+
+
 
 
 
