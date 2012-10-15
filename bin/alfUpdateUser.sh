0910bfb9e69e52a660e63c8d0b05e0657e00c9d6
Lothar Maerkle <lothar.maerkle@ecm4u.de>
2012-10-15 16:54:24 +0200

    change basic properties

--- bin/alfUpdateUser.sh
+++ bin/alfUpdateUser.sh
@@ -1,4 +1,130 @@
 #!/bin/bash
+# set -x
+# param section
+
+# source function library
+
+ALFTOOLS_BIN=`dirname "$0"`
+. $ALFTOOLS_BIN/alfToolsLib.sh
 
 
+
+function __show_command_options() {
+  echo "  command options:"
+  echo "    -n    Alresco use id"
+  echo "    -f    Users first name"
+  echo "    -l    Users last name"
+  echo "    -e    Users email"
+  echo 
+}
+
+
+# intended to be replaced in command script
+function __show_command_explanation() {
+  echo "  command explanation:"
+  echo "    the alfUpdateUser.sh scripts updates the core user properties like first and last name"
+  echo
+  echo "  usage examples:"
+  echo "    alfUpateUser.sh -n lothar -f Lodda"
+  echo "        ---> Changes the first name of the user 'lothar' to 'Lodda'"
+  echo 
+}
+
+ALF_CMD_OPTIONS="${ALF_GLOBAL_OPTIONS}n:p:f:l:e:g:"
+ALF_USER_NAME=""
+ALF_FIRST_NAME=""
+ALF_LAST_NAME=""
+ALF_EMAIL=""
+ALF_PASSWD=""
+ALF_GROUPS=()
+
+function __process_cmd_option() {
+  local OPTNAME=$1
+  local OPTARG=$2
+
+  case $OPTNAME
+  in
+    n)
+      ALF_USER_NAME=$OPTARG;;
+    f)
+      ALF_FIRST_NAME=$OPTARG;;
+    l)
+      ALF_LAST_NAME=$OPTARG;;
+    e)
+      ALF_EMAIL=$OPTARG;;
+    p)
+      ALF_PASSWD=$OPTARG;;
+    g)
+      ALF_GROUPS=("${ALF_GROUPS[@]}" $OPTARG);;
+  esac
+}
+
+__process_options "$@"
+
+# shift away parsed args
+shift $((OPTIND-1))
+
+if $ALF_VERBOSE
+then
+  ALF_CURL_OPTS="$ALF_CURL_OPTS -v"
+  echo "connection params:"
+  echo "  user: $ALF_UID"
+  echo "  endpoint: $ALF_EP"
+  echo "  curl opts: $ALF_CURL_OPTS"
+  echo "  user name: $ALF_USER_NAME"
+  echo "  first name: $ALF_FIRST_NAME"
+  echo "  last name: $ALF_LAST_NAME"
+  echo "  email: $ALF_EMAIL"
+fi
+
+ALF_JSON='{}'
+
+if [[ "$ALF_USER_NAME" == "" ]]
+then
+  echo "an Alfresco user name is required"
+  exit 1
+fi
+
+if [[ "$ALF_FIRST_NAME" != "" ]]
+then
+  ALF_JSON=`echo "$ALF_JSON"| $ALF_JSHON -s "$ALF_FIRST_NAME" -i firstName`
+fi
+
+if [[ "$ALF_LAST_NAME" != "" ]]
+then
+  ALF_JSON=`echo "$ALF_JSON"| $ALF_JSHON -s "$ALF_LAST_NAME" -i lastName`
+fi
+
+if [[ "$ALF_EMAIL" != "" ]]
+then
+  ALF_JSON=`echo "$ALF_JSON"| $ALF_JSHON -s "$ALF_EMAIL" -i email`
+fi
+
+if [[ "$ALF_JSON" == "{}" ]]
+then
+  echo "at least first name, last name or email as to be set."
+  exit 1
+fi
+
+# TODO Groups
+
+# set groups if any
+for GRP in ${ALF_GROUPS[*]}
+do
+  ALF_AUTHORITY="GROUP_${GRP}"
+  ALF_JSON=`echo $ALF_JSON | $ALF_JSHON -e groups -s "$ALF_AUTHORITY" -i append -p`  	
+done
+
+#echo $ALF_JSON
+
+__encode_url_param $ALF_USER_NAME
+ALF_ENC_UID=$ENCODED_PARAM
+
+echo $ALF_JSON | curl $ALF_CURL_OPTS -u $ALF_UID:$ALF_PW -H 'Content-Type:application/json' -d@- -X PUT $ALF_EP/service/api/people/$ALF_ENC_UID
+
+
+#{"userName":"lodda","password":"test","firstName":"Lothar","lastName":"MÃ¤rkle","email":"lothar.maerkle@ecm4u.de","disableAccount":false,"quota":-1,"groups":[]}
+#
+#
+#http://localhost:8080/share/proxy/alfresco/api/people
 
